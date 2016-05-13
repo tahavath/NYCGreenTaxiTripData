49b0f8afdbd6a99981612ea80d3712b045e9e1e4
tahavath <krkt@mac-4611.local>
2016-05-13 13:19:41 +0200

    trip list table view added

---
+++ NYCGreenTaxiTripData/View/TripsListTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TripsListTableViewController.h
+//  NYCGreenTaxiTripData
+//
+//  Created by KRKT on 13/05/16.
+//  Copyright © 2016 tahavath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+extern NSString *const THVTripCellIdentifier;
+
+@interface TripsListTableViewController : UITableViewController
+
+@end
